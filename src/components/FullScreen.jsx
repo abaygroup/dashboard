b9dd84bbaf9afb0b12f5df3101ae067a41246451
Mukhagali Aryn <mukhagaliaryn@gmail.com>
2021-08-04 11:35:59 +0600

    Done

--- src/components/FullScreen.jsx
+++ src/components/FullScreen.jsx
@@ -7,17 +7,76 @@ const LoaderContainer = styled.div`
     top: 0; left: 0;
     width: 100%;
     height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     background: ${({theme}) => theme.background};
     color: ${({theme}) => theme.color};
+
+
+    .loading-box {
+        position: absolute;
+        top: 40%; left: 50%;
+
+        div {
+            position: absolute;
+            top: 33px;
+            width: 13px;
+            height: 13px;
+            border-radius: 50%;
+            background: #D8D8D7;
+            animation-timing-function: cubic-bezier(0, 1, 1, 0);
+    
+            &:nth-child(1) {
+                left: 8px;
+                animation: lds-ellipsis1 0.6s infinite;
+            }
+            &:nth-child(2) {
+                left: 8px;
+                animation: lds-ellipsis2 0.6s infinite;
+            }
+            &:nth-child(3) {
+                left: 32px;
+                animation: lds-ellipsis2 0.6s infinite;
+            }
+            &:nth-child(4) {
+                left: 56px;
+                animation: lds-ellipsis3 0.6s infinite;
+            }
+        }
+    
+        @keyframes lds-ellipsis1 {
+            0% {
+            transform: scale(0);
+            }
+            100% {
+            transform: scale(1);
+            }
+        }
+        @keyframes lds-ellipsis3 {
+            0% {
+            transform: scale(1);
+            }
+            100% {
+            transform: scale(0);
+            }
+        }
+        @keyframes lds-ellipsis2 {
+            0% {
+            transform: translate(0, 0);
+            }
+            100% {
+            transform: translate(24px, 0);
+            }
+        }
+    }
 `;
 
 const FullLoader = () => {
     return (
         <LoaderContainer>
-            <p>Загрузка...</p>
+            <div className="loading-box">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </LoaderContainer>
     )
 }
